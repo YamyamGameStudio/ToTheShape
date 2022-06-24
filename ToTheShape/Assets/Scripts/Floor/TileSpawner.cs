a3498da8ca025fa5a20f47b9013dd6b26e0d7219
Olcay ÇİFTÇİ <olcaycft95@gmail.com>
2022-06-24 22:54:23 +0300

    Update TileSpawner.cs

--- ToTheShape/Assets/Scripts/Floor/TileSpawner.cs
+++ ToTheShape/Assets/Scripts/Floor/TileSpawner.cs
@@ -1,28 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Olcay
 {
-    public class TileSpawner : MonoBehaviour
+    public class TileSpawner : MonoSingleton<TileSpawner>
     {
         [SerializeField] private GameObject tilePrefab;
         [SerializeField] private Transform tileSpawnPoint;
-        private List<GameObject> TilesList = new List<GameObject>();
+        private List<GameObject> tilesList = new List<GameObject>();
 
         private void Start()
         {
-            SpawnTile();
+            SpawnTile(5);
         }
 
-
-        private void SpawnTile()
+        //object pool ile değişecek
+        private void SpawnTile(int count)
         {
-            for (int i = 0; i < 5; i++)
+            
+            for (int i = 0; i < count; i++)
             {
                 var tile = Instantiate(tilePrefab, tileSpawnPoint.position, tilePrefab.transform.rotation);
                 tileSpawnPoint = tile.transform.GetChild(1).transform;
+                tilesList.Add(tile);
             }
+        }
+
+        //object pool ile değişecek
+        public void DestroyTilePointAction()
+        {
+            var firstTile=tilesList.First();
+            tilesList.Remove(firstTile);
+            Destroy(firstTile);
+            
+            SpawnTile(1);
         }
     }
 }
